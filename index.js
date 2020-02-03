f468d083910dc3e28d320915d4e1cdd2d30cb8a3
drbarq <J.Tustin@gmail.com>
2020-02-03 16:09:05 -0700

    new solution

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 function findMinAndRemove(array){
+    let min = array[0]
+    let minIndex = 0
 
+    for (let i = 0; i < array.length; i++) {
+        let currentElement = array[i]
+        if (currentElement < min) {
+            min = currentElement
+            minIndex = i
+        }
+    }
+    array.splice(minIndex, 1)
+    return min
 }
 
 function insertionSort(array){
+    let newMin
+    let sorted = []
 
+    while (array.length != 0) {
+        newMin = findMinAndRemove(array)
+        sorted.push(newMin)
+    }
+    return sorted
 }
+
+
+
+// let array1 = [2,3,4,5,6,7]
+// let array2 = [ 3, 2, 4, 5, 6, 7 ]
+
+// findMinAndRemove(array1)
